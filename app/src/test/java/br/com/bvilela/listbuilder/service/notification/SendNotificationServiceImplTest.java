efaf31b4583a03db4cc6edbb302254c48e13aecb
Bruno Vilela <brunovilela2008@hotmail.com>
2023-06-08 23:03:03 -0300

    Update SendNotificationServiceImplTest.java

--- app/src/test/java/br/com/bvilela/listbuilder/service/notification/SendNotificationServiceImplTest.java
+++ app/src/test/java/br/com/bvilela/listbuilder/service/notification/SendNotificationServiceImplTest.java
@@ -1,34 +1,35 @@
 package br.com.bvilela.listbuilder.service.notification;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-
+import br.com.bvilela.lib.model.CalendarEvent;
 import br.com.bvilela.lib.service.GoogleCalendarCreateService;
 import br.com.bvilela.listbuilder.builder.VidaCristaExtractWeekDtoBuilder;
-import br.com.bvilela.listbuilder.config.NotifyProperties;
+import br.com.bvilela.listbuilder.builder.designacao.DesignacaoWriterDtoBuilder;
+import br.com.bvilela.listbuilder.dto.designacao.writer.DesignacaoWriterDTO;
 import br.com.bvilela.listbuilder.dto.limpeza.FinalListLimpezaDTO;
-import br.com.bvilela.listbuilder.dto.limpeza.FinalListLimpezaItemDTO;
 import br.com.bvilela.listbuilder.dto.limpeza.FinalListLimpezaItemLayout2DTO;
-import br.com.bvilela.listbuilder.dto.vidacrista.VidaCristaExtractWeekDTO;
-import br.com.bvilela.listbuilder.exception.ListBuilderException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.SneakyThrows;
-import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 class SendNotificationServiceImplTest {
 
     @InjectMocks private SendNotificationServiceImpl service;
-
-    @InjectMocks private NotifyProperties notifProperties; // TODO: remove
 
     @Mock private NotifyDesignationServiceImpl notifyDesignationService;
 
@@ -40,11 +41,15 @@ class SendNotificationServiceImplTest {
 
     @Mock private GoogleCalendarCreateService calendarService;
 
-    private static final int LIMPEZA_LAYOUT1 = 1;
-    private static final int LIMPEZA_LAYOUT2 = 2;
+    private static final CalendarEvent calendarEvent = CalendarEvent
+            .builder()
+            .setSummary("test")
+            .setDateTimeStart(LocalDateTime.now())
+            .setDateTimeEnd(LocalDateTime.now().plusHours(1))
+            .build();
 
-    private final FinalListLimpezaDTO dtoLimpeza =
-            FinalListLimpezaDTO.builder()
+    private final FinalListLimpezaDTO clearingDTO =
+            FinalListLimpezaDTO .builder()
                     .itemsLayout2(
                             List.of(
                                     FinalListLimpezaItemLayout2DTO.builder()
@@ -63,11 +68,6 @@ class SendNotificationServiceImplTest {
                                             .build()))
                     .build();
 
-    private final List<LocalDate> dtoAssistencia = List.of(LocalDate.now());
-
-    private final List<VidaCristaExtractWeekDTO> dtoVidaCrista =
-            List.of(VidaCristaExtractWeekDtoBuilder.create().withRandomDataOneMonth().build());
-
     @BeforeEach
     void setupBeforeEach() {
         MockitoAnnotations.openMocks(this);
@@ -80,172 +80,85 @@ class SendNotificationServiceImplTest {
                         calendarService);
     }
 
-    // *********************** ASSISTENCIA *********************** \\
-    @Test
-    void shouldAssitenciaNotifActiveFalse() {
-        assertDoesNotThrow(() -> service.assistencia(dtoAssistencia));
+
+    // *********************** CLEARING *********************** \\
+    @DisplayName("Send Clearing Events: Events is Null/Empty")
+    @ParameterizedTest(name = "Events is \"{0}\"")
+    @NullAndEmptySource
+    void clearingEventsNullEmpty(List<CalendarEvent> events) {
+        int CLEARING_LAYOUT1 = 1;
+        when(notifyClearingService.createEvents(any(FinalListLimpezaDTO.class), any(Integer.class))).thenReturn(events);
+        service.limpeza(clearingDTO, CLEARING_LAYOUT1);
+        verify(calendarService, never()).createEvents(events);
     }
 
     @Test
-    void shouldAssitenciaNotifActiveTrue() {
-        setNotifyActive();
-        assertDoesNotThrow(() -> service.assistencia(dtoAssistencia));
+    void clearingEventsNotNullEmpty() {
+        int CLEARING_LAYOUT2 = 2;
+        List<CalendarEvent> events = List.of(calendarEvent);
+        when(notifyClearingService.createEvents(any(FinalListLimpezaDTO.class), any(Integer.class))).thenReturn(events);
+        service.limpeza(clearingDTO, CLEARING_LAYOUT2);
+        verify(calendarService, times(1)).createEvents(events);
     }
 
-    // *********************** LIMPEZA *********************** \\
-    @Test
-    void shouldLimpezaNotifActiveFalse() {
-        assertDoesNotThrow(() -> service.limpeza(dtoLimpeza, LIMPEZA_LAYOUT2));
-    }
 
-    /* TODO: mover classe correta
+    // *********************** AUDIENCE *********************** \\
     @Test
-    void shouldLimpezaNotifActiveTrueNotifNameNull() {
-        setNotifyActive();
-        assertThrows(
-                ListBuilderException.class, () -> service.limpeza(dtoLimpeza, LIMPEZA_LAYOUT2));
-    }
-    */
-
-    @Test
-    void shouldAssistenciaNotifActiveTrueWithoutNotifPersonSuccess() {
-        setNotifyActive();
-        setNotifyName("xpto");
-        assertDoesNotThrow(() -> service.limpeza(dtoLimpeza, LIMPEZA_LAYOUT2));
+    void audienceEventsNull() {
+        var dtoAssistencia = List.of(LocalDate.now());
+        when(notifyAudienceService.createEvent(anyList())).thenReturn(null);
+        service.assistencia(dtoAssistencia);
+        verify(calendarService, never()).createEvent(null);
     }
 
     @Test
-    void shouldAssistenciaNotifActiveTrueNotifPersonSuccess() {
-        setNotifyActive();
-        setNotifyName("P1");
-        assertDoesNotThrow(() -> service.limpeza(dtoLimpeza, LIMPEZA_LAYOUT2));
+    void audienceEventsNotNull() {
+        var dtoAssistencia = List.of(LocalDate.now());
+        when(notifyAudienceService.createEvent(anyList())).thenReturn(calendarEvent);
+        service.assistencia(dtoAssistencia);
+        verify(calendarService, times(1)).createEvent(calendarEvent);
     }
 
-    @Test
-    void shouldLimpezaNotifActiveTrueNotifPersonLayout1Success() {
-        var item = new FinalListLimpezaItemDTO(LocalDate.of(2022, 6, 10), "Label", "P1, P2, P3");
-        final FinalListLimpezaDTO dtoLimpezaLayout1 =
-                FinalListLimpezaDTO.builder().items(List.of(item)).build();
-        setNotifyActive();
-        setNotifyName("P1");
-        assertDoesNotThrow(() -> service.limpeza(dtoLimpezaLayout1, LIMPEZA_LAYOUT1));
-    }
 
-    @Test
-    void shouldLimpezaNotifActiveGroupNull() {
-        setNotifyActive();
-        setNotifyChristianLifeMeetingDay("terca");
-        var item = dtoLimpeza.getItemsLayout2().get(0);
-        var newItem =
-                FinalListLimpezaItemLayout2DTO.builder()
-                        .withDate1(item.getDate1())
-                        .withDate2(item.getDate2())
-                        .withGroup(null)
-                        .withLabel1(item.getLabel1())
-                        .withLabel2(item.getLabel2())
-                        .build();
-        List<FinalListLimpezaItemLayout2DTO> newList =
-                new ArrayList<>(dtoLimpeza.getItemsLayout2());
-        newList.add(newItem);
-        var newDtoLimpeza = FinalListLimpezaDTO.builder().itemsLayout2(newList).build();
-        setNotifyName("XPTO");
-        assertDoesNotThrow(() -> service.limpeza(newDtoLimpeza, LIMPEZA_LAYOUT2));
-    }
-
-    @Test
-    @SneakyThrows
-    void shouldLimpezaNotifActiveTrueNotifPersonPreMeetingSuccess() {
-        setNotifyActive();
-        setNotifyName("P1");
-        FieldUtils.writeField(notifProperties, "notifyCleaningPreMeeting", true, true);
-        assertDoesNotThrow(() -> service.limpeza(dtoLimpeza, LIMPEZA_LAYOUT2));
-    }
-
-    // *********************** VIDA CRISTA *********************** \\
-    @Test
-    void shouldVidaCristaNotifActiveFalse() {
-        var list =
+    // *********************** CHRISTIAN LIFE *********************** \\
+    @DisplayName("Send Clearing Events: Events is Null/Empty")
+    @ParameterizedTest(name = "Events is \"{0}\"")
+    @NullAndEmptySource
+    void christianLifeEventsNullEmpty(List<CalendarEvent> events) {
+        var dtoVidaCrista =
                 List.of(VidaCristaExtractWeekDtoBuilder.create().withRandomDataOneMonth().build());
-        assertDoesNotThrow(() -> service.vidaCrista(list));
-    }
-
-    /* TODO: mover classe correta
-    @Test
-    void shouldVidaCristaNotifActiveTrueNotifNameNull() {
-        setNotifyActive();
-        assertThrows(ListBuilderException.class, () -> service.vidaCrista(dtoVidaCrista));
-    }
-    */
-
-    @Test
-    void shouldVidaCristaNotifActiveTrueWithoutNotifSuccess() {
-        setNotifyActive();
-        setNotifyChristianLifeMeetingDay("terca");
-        setNotifyName("P1");
-        assertDoesNotThrow(() -> service.vidaCrista(dtoVidaCrista));
+        when(notifyChristianLifeService.createEvents(anyList())).thenReturn(events);
+        service.vidaCrista(dtoVidaCrista);
+        verify(calendarService, never()).createEvents(events);
     }
 
     @Test
-    void shouldVidaCristaNotifActiveTrueNotifSuccess() {
-        setNotifyActive();
-        setNotifyChristianLifeMeetingDay("terca");
-        var name = dtoVidaCrista.get(0).getItems().get(0).getParticipants().get(0);
-        setNotifyName(name);
-        assertDoesNotThrow(() -> service.vidaCrista(dtoVidaCrista));
+    void christianLifeEventsNotNullEmpty() {
+        var dtoVidaCrista =
+                List.of(VidaCristaExtractWeekDtoBuilder.create().withRandomDataOneMonth().build());
+        List<CalendarEvent> events = List.of(calendarEvent);
+        when(notifyChristianLifeService.createEvents(anyList())).thenReturn(events);
+        service.vidaCrista(dtoVidaCrista);
+        verify(calendarService, times(1)).createEvents(events);
+    }
+
+
+    // *********************** DESIGNATION *********************** \\
+    @Test
+    void designationEventsNull() {
+        var designationDTO = DesignacaoWriterDtoBuilder.create().withRandomData().build();
+        when(notifyDesignationService.createEvents(any(DesignacaoWriterDTO.class))).thenReturn(null);
+        service.designacao(designationDTO);
+        verify(calendarService, never()).createEvent(null);
     }
 
     @Test
-    void shouldVidaCristaNotifActiveTrueButNameItemNull() {
-        setNotifyActive();
-        setNotifyChristianLifeMeetingDay("terca");
-        dtoVidaCrista.get(0).getItems().get(0).setParticipants(null);
-        setNotifyName("XPTO");
-        assertDoesNotThrow(() -> service.vidaCrista(dtoVidaCrista));
+    void designationEventsNotNull() {
+        var designationDTO = DesignacaoWriterDtoBuilder.create().withRandomData().build();
+        List<CalendarEvent> events = List.of(calendarEvent);
+        when(notifyDesignationService.createEvents(any(DesignacaoWriterDTO.class))).thenReturn(events);
+        service.designacao(designationDTO);
+        verify(calendarService, times(1)).createEvents(events);
     }
 
-    /* TODO: mover classe correta
-    @Test
-    void shouldVidaCristaMidweekMeetingDayNullException() {
-        setNotifyActive();
-        var name = dtoVidaCrista.get(0).getItems().get(0).getParticipants().get(0);
-        setNotifyName(name);
-        setNotifyChristianLifeMeetingDay("teste");
-        var exception =
-                assertThrows(ListBuilderException.class, () -> service.vidaCrista(dtoVidaCrista));
-        assertEquals(
-                "Propriedade 'notify.christianlife.midweek.meeting.day' não é um Dia da Semana Válido!",
-                exception.getMessage());
-    }
-
-    @Test
-    void shouldVidaCristaMidweekMeetingDayInvalidException() {
-        setNotifyActive();
-        var name = dtoVidaCrista.get(0).getItems().get(0).getParticipants().get(0);
-        setNotifyName(name);
-        var exception =
-                assertThrows(ListBuilderException.class, () -> service.vidaCrista(dtoVidaCrista));
-        assertEquals(
-                "Defina a propriedade 'notify.christianlife.midweek.meeting.day'!",
-                exception.getMessage());
-    }
-    */
-
-    // *********************** DESIGNACAO *********************** \\
-
-
-    // *********************** UTILS *********************** \\
-    @SneakyThrows
-    private void setNotifyActive() {
-        FieldUtils.writeField(notifProperties, "notifyActive", true, true);
-    }
-
-    @SneakyThrows
-    private void setNotifyChristianLifeMeetingDay(String value) {
-        FieldUtils.writeField(notifProperties, "notifyChristianLifeMeetingDay", value, true);
-    }
-
-    @SneakyThrows
-    private void setNotifyName(String name) {
-        FieldUtils.writeField(notifProperties, "notifyName", name, true);
-    }
 }
