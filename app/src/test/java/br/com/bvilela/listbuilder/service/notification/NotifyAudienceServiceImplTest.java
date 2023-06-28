3e4d321e8382b3e5f7e85c1936f03671a2b5cc7a
Bruno Vilela <brunovilela2008@hotmail.com>
2023-06-28 17:20:00 -0300

    Create NotifyAudienceServiceImplTest.java

---
+++ app/src/test/java/br/com/bvilela/listbuilder/service/notification/NotifyAudienceServiceImplTest.java
@@ -0,0 +1,49 @@
+package br.com.bvilela.listbuilder.service.notification;
+
+import br.com.bvilela.listbuilder.config.NotifyProperties;
+import br.com.bvilela.listbuilder.service.notification.impl.NotifyAudienceServiceImpl;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class NotifyAudienceServiceImplTest {
+    @InjectMocks
+    private NotifyAudienceServiceImpl service;
+    @InjectMocks private NotifyProperties properties;
+
+    @SneakyThrows
+    @BeforeEach
+    void setupBeforeEach() {
+        MockitoAnnotations.openMocks(this);
+        service = new NotifyAudienceServiceImpl(properties);
+    }
+
+    @SneakyThrows
+    private void setNotifyActive(boolean value) {
+        FieldUtils.writeField(properties, "notifyActive", value, true);
+    }
+
+    @Test
+    void createEventNotifyInactive() {
+        setNotifyActive(false);
+        var event = service.createEvent(List.of(LocalDate.now()));
+        assertNull(event);
+    }
+
+    @Test
+    void createEventNotifyActive() {
+        setNotifyActive(true);
+        var event = service.createEvent(List.of(LocalDate.now()));
+        assertNotNull(event);
+    }
+
+}
