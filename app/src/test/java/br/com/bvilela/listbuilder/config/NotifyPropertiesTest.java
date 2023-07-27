355969186f7fc9bde994330476ce1fd549f9863d
Bruno Vilela <brunovilela2008@hotmail.com>
2023-07-26 21:11:23 -0300

    ajuste pmd

--- app/src/test/java/br/com/bvilela/listbuilder/config/NotifyPropertiesTest.java
+++ app/src/test/java/br/com/bvilela/listbuilder/config/NotifyPropertiesTest.java
@@ -13,7 +13,10 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NotifyPropertiesTest {
 
