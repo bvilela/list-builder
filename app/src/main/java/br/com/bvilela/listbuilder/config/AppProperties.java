374ce1923f8e7f1dd54e35632804c109fc2f4475
Bruno Vilela <brunovilela2008@hotmail.com>
2023-08-20 20:17:16 -0300

    chore: forte PMD Violation

--- app/src/main/java/br/com/bvilela/listbuilder/config/AppProperties.java
+++ app/src/main/java/br/com/bvilela/listbuilder/config/AppProperties.java
@@ -1,6 +1,7 @@
 package br.com.bvilela.listbuilder.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
