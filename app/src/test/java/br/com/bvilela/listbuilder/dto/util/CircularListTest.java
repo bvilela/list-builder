4e5470a47539b4c61e73d9885d16d2db743f2c1c
Bruno Vilela <brunovilela2008@hotmail.com>
2023-09-27 22:48:58 -0300

    fix: circular list

---
+++ app/src/test/java/br/com/bvilela/listbuilder/dto/util/CircularListTest.java
@@ -0,0 +1,41 @@
+package br.com.bvilela.listbuilder.dto.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CircularListTest {
+
+    private final String P1 = "Person1";
+    private final String P2 = "Person2";
+    private final String P3 = "Person3";
+    private final String P4 = "Person4";
+    private final String P5 = "Person5";
+
+    private final List<String> list = List.of(P1, P2, P3, P4, P5);
+
+    @Test
+    void circularListInitialIndexFirst() {
+        CircularList<String> circularList = new CircularList<>(list,0);
+        assertEquals(P2, circularList.next());
+        assertEquals(P3, circularList.next());
+        assertEquals(P4, circularList.next());
+        assertEquals(P5, circularList.next());
+        assertEquals(P1, circularList.next());
+    }
+
+    @Test
+    void circularListInitialIndexLast() {
+        CircularList<String> circularList = new CircularList<>(list,list.size()-1);
+        assertEquals(P1, circularList.next());
+        assertEquals(P2, circularList.next());
+        assertEquals(P3, circularList.next());
+        assertEquals(P4, circularList.next());
+        assertEquals(P5, circularList.next());
+        assertEquals(P1, circularList.next());
+    }
+
+}
